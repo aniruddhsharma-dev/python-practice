b6fdef80a073b6ed594ffe784ed1166daf9185a8
Aniruddh Sharma <aniruddh.sharma.dev@gmail.com>
2026-02-26 16:03:39 +0530

    Added basic Python practice programs

---
+++ 1practice.py
@@ -0,0 +1,7 @@
+print("Program to input 2 numbers and print their sum")
+a = int(input("Enter the first nmbber : "))
+b = int(input("Enter the second nmbber : "))
+print("a = ",a)
+print("b =",b)
+sum = a+b
+print("The sum of two numbers are : ",sum)
---
+++ length of name.py
@@ -0,0 +1,4 @@
+name = input("Enter your name : ")
+print(name)
+length = len(name)
+print("The length of the name is :",length)
