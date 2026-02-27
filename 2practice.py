481513a4062f004f93396b3dda9e849a9f9881d7
Aniruddh Sharma <aniruddh.sharma.dev@gmail.com>
2026-02-27 15:12:14 +0530

    Delete 2practice.py

--- 2practice.py
+++
@@ -1,6 +0,0 @@
-print("Find Area of Square")
-sides = float(input("Enter the length of side : "))
-print("side =",sides)
-print("Formula for Area of square : A = S*S")
-area = sides * sides
-print("The area of square is :",area)
