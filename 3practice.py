f0c3c3762c2df3b12cc97465e3208933db68240f
Aniruddh Sharma <aniruddh.sharma.dev@gmail.com>
2026-02-27 15:12:29 +0530

    Delete 3practice.py

--- 3practice.py
+++
@@ -1,7 +0,0 @@
-print("Input 2 floating point and finding the average")
-a = float(input("Enter the first number : "))
-b = float(input("Enter the second number : "))
-print("a = ",a)
-print("b = ",b)
-Average = (a + b) / 2
-print("Average of 2 numbers is : ",Average)
